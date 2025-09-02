f3d492c73f008899426a06ade429262159009da9
Gandhi, Abheek <abheek.gandhi@intel.com>
2025-09-02 09:37:03 -0700

    Added readme and file to run inference

---
+++ tools/genai_inference/README.md
@@ -0,0 +1,71 @@
+# Python genai sample
+
+This example showases inference of several categories of models including large language models and visual language models. The application doesn't have many configuration options to encourage the reader to explore and modify the source code. For example, change the weight format on the export to int4 or fp16.
+
+The code given uses the openvino.genai repo to run the following two benchmarks:
+ - [`benchmark.py`](../llm_bench/benchmark.py) demonstrates basic usage of the LLM pipeline.
+ - [`benchmark_vlm.py`](../../samples/python/benchmark_vlm.py) shows how to benchmark a VLM in OpenVINO GenAI. The script includes functionality for warm-up iterations, generating text and calculating various performance metrics.
+
+## Run benchmark:
+
+```sh
+python run_genai_inference.py [OPTIONS]
+```
+
+### Options
+
+- `-m, --model`(default: `None`): Path to the model and tokenizers base directory.
+- `-c, --category` (default: `None`): The category of models, either choose from vlm or llm.`
+- `--input` (default: `1024`): Size of input prompt file, can choose from sizes included in prompts directory under ./tools/llm_bench/prompts.
+- `--output` (default: `128`): Output token count, maximum tokens generated will not exceed this number.
+- `--height` (default: `512`): Image resolution height. For visual language models only.
+- `--width` (default: `512`): Image resolution width. For visual language models only.
+
+### Output:
+
+```
+python run_genai_inference.py -c vlm -m minicpm-v
+```
+
+```
+res= The sky appears blue because of a natural phenomenon called Rayleigh scattering. When sunlight reaches the Earth's atmosphere, it encounters molecules like nitrogen and oxygen. These molecules scatter the shorter, blue wavelengths of light more than the longer, red wavelengths. This scattering makes the sky appear blue, especially during the day when the sun is shining brightly.
+Load time: 7822.00 ms
+Generate time: 832.31 ± 4.91 ms
+Tokenization time: 2.06 ± 0.14 ms
+Detokenization time: 0.29 ± 0.01 ms
+Embeddings preparation time: 65.93 ± 0.00 ms
+TTFT: 106.16 ± 1.58 ms
+TPOT: 10.83 ± 2.30 ms/token 
+Throughput: 92.32 ± 19.64 tokens/s
+```
+
+### List of Supported Models
+
+```
+LLM Models Supported:
+llama2 (7b, int4)
+llama3.2 (3b, int4)
+llama3.1 (8b, int4)
+GLM-Edge (4b, int4)
+Qwen2.5-7B chat (int4)
+Qwen3-0.6B base (FP16)
+Qwen3-8B base (int4)
+Phi-3.5-mini(3.8b, int4)
+Phi4 mini reasoning 3.8B  (int4)
+Phi4 mini instruct 3.8B (int4)
+Gemma1 (7b, int4)
+Mistral 7b (int4)
+MiniCPM1-1B (FP16)
+DeepSeek R1 (Qwen 14b, int4)
+```
+
+```
+VLM Models Supported:
+gemma 3 4b (int4)
+miniCPM-V2.6 (Qwen2-7B, int4)
+miniCPM-o2.6 (Qwen2-7B, int4)
+llava3-next-llama3 (Llama-3-8B-Instruct, int4)
+LLaVA-NeXT-Video-7B-hf(int4)
+phi3.5 vision (fp16)
+phi4 vision (fp16)
+```
---
+++ tools/genai_inference/run_genai_inference.py
@@ -0,0 +1,178 @@
+import logging as log
+import sys
+import argparse
+import os
+import venv
+
+def initialize_repo_and_env(llava_video=False):
+    # getting the pip and python from the venv
+    VENV_DIR = "openvino-env"
+    if llava_video:
+        VENV_DIR = "llava-video-env"
+    pip_exe = os.path.join(os.getcwd(), VENV_DIR, "Scripts", "pip.exe")
+    python_exe = os.path.join(os.getcwd(), VENV_DIR, "Scripts", "python.exe")
+
+    # If user not in venv, creates the venv and activates
+    if sys.prefix == sys.base_prefix:
+        print("Not in a venv, so will create and activate.")
+
+        VENV_DIR = "openvino-env"
+        if llava_video:
+            VENV_DIR = "llava-video-env"
+        # Creating the venv if it does not exist
+        if not os.path.exists(VENV_DIR):
+            venv.create(VENV_DIR, with_pip=True)
+
+
+        pip_exe = os.path.join(os.getcwd(), VENV_DIR, "Scripts", "pip.exe")
+        python_exe = os.path.join(os.getcwd(), VENV_DIR, "Scripts", "python.exe")
+
+        os.execv(python_exe, [python_exe] + sys.argv)
+    
+    print("Activated venv")
+    print(pip_exe)
+
+    entries = os.listdir(os.getcwd())
+
+    # If openvino.genai repo already exists, navigate into correct directory
+    if "openvino.genai" in entries:
+        print("Already found openvino.genai, Assuming repo has been setup properly...")
+        os.chdir("openvino.genai")
+        os.chdir("tools")
+        os.chdir("llm_bench")
+    # Otherwise, clones the correct repo and then installs requirements
+    else:
+        os.system("git clone https://github.com/abheekga/openvino.genai.git")
+        os.chdir("openvino.genai")
+        os.chdir("tools")
+        os.chdir("llm_bench")
+        
+        print("Installing the requirements from the llm_bench")
+        cwd = os.getcwd()
+        requirements_file = os.path.join(cwd, "openvino_requirements.txt")
+        if llava_video:
+            requirements_file = os.path.join(cwd, "llava_video_requirements.txt")
+
+        if os.path.exists(requirements_file):
+            print("Found req file")
+        else:
+            print(f"Files here: {os.listdir(cwd)}")
+
+        os.system(f"\"{python_exe}\" -m pip install --upgrade pip")
+        os.system(f"\"{pip_exe}\" install -r {requirements_file}")
+        print("Requirements were installed.")
+
+
+    print("Inside the repo and entered llm_bench, ready for inference")
+
+    return python_exe
+
+def initialize_repo_and_env_minicpm_o():
+    # getting the pip and python from the venv
+    VENV_DIR = "minicpm-o-env"
+    pip_exe = os.path.join(os.getcwd(), VENV_DIR, "Scripts", "pip.exe")
+    python_exe = os.path.join(os.getcwd(), VENV_DIR, "Scripts", "python.exe")
+
+    # If user not in venv, creates the venv and activates
+    if sys.prefix == sys.base_prefix:
+        print("Not in a venv, so will create and activate.")
+
+        VENV_DIR = "minicpm-o-env"
+        # Creating the venv if it does not exist
+        if not os.path.exists(VENV_DIR):
+            venv.create(VENV_DIR, with_pip=True)
+
+
+        pip_exe = os.path.join(os.getcwd(), VENV_DIR, "Scripts", "pip.exe")
+        python_exe = os.path.join(os.getcwd(), VENV_DIR, "Scripts", "python.exe")
+
+        os.execv(python_exe, [python_exe] + sys.argv)
+    
+    print("Activated venv")
+    print(pip_exe)
+
+    entries = os.listdir(os.getcwd())
+
+    # If openvino.genai repo already exists, navigate into correct directory
+    if "openvino.genai" in entries:
+        print("Already found openvino.genai, Assuming repo has been setup properly...")
+        os.chdir("openvino.genai")
+        os.chdir("tools")
+        os.chdir("llm_bench")
+        os.chdir("minicpm-o")
+    # Otherwise, clones the correct repo and then installs requirements
+    else:
+        os.system("git clone https://github.com/abheekga/openvino.genai.git")
+        os.chdir("openvino.genai")
+        os.chdir("tools")
+        os.chdir("llm_bench")
+        os.chdir("minicpm-o")
+        
+        print("Installing the requirements from the minicpm-o")
+        cwd = os.getcwd()
+        requirements_file = os.path.join(cwd, "requirements.txt")
+
+        if os.path.exists(requirements_file):
+            print("Found req file")
+        else:
+            print(f"Files here: {os.listdir(cwd)}")
+
+        os.system(f"\"{python_exe}\" -m pip install --upgrade pip")
+        os.system(f"\"{pip_exe}\" install -r {requirements_file}")
+        print("Requirements were installed.")
+
+
+    print("Inside the repo and entered llm_bench, ready for inference")
+
+    return python_exe
+
+def main(args):
+    log.basicConfig(format='[ %(levelname)s ] %(message)s', level=log.INFO, stream=sys.stdout)
+
+    if args.category == "vlm" and args.model == "minicpm-o":
+        python_exe = initialize_repo_and_env_minicpm_o()
+        scripts_dir = os.path.dirname(python_exe)
+
+        # Setting the correct environment path to properly use the venv packages
+        os.environ["PATH"] = f"{scripts_dir};{os.environ.get('PATH','')}"
+        
+        ret = os.system(f"{python_exe} minicpm-o-omnimodal-chatbot.py")
+        if ret != 0:
+            sys.exit(ret)
+
+        sys.exit(0)
+    else:
+        if args.category == "vlm" and args.model == "llava-video":
+            python_exe = initialize_repo_and_env(llava_video=True)
+        else:
+            python_exe = initialize_repo_and_env()
+        scripts_dir = os.path.dirname(python_exe)
+
+        # Setting the correct environment path to properly use the venv packages
+        os.environ["PATH"] = f"{scripts_dir};{os.environ.get('PATH','')}"
+
+        # Depending on the category of user request, runs the proper command
+        if args.category == "llm":
+            ret = os.system(f"{python_exe} llm-pipeline-ov.py -m {args.model} --input {args.input} --output {args.output}")
+            if ret != 0:
+                sys.exit(ret)
+        elif args.category == "vlm":
+            ret = os.system(f"{python_exe} vlm-pipeline-ov.py -m {args.model} --input {args.input} --output {args.output} --height {args.height} --width {args.width}")
+            if ret != 0:
+                sys.exit(ret)
+
+        sys.exit(0)
+    
+
+
+   
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--model", "-m", required=True)
+    parser.add_argument("--category", "-c", required=True)
+    parser.add_argument("--input", default=1024)
+    parser.add_argument("--output", default=128)
+    parser.add_argument("--height", default=512)
+    parser.add_argument("--width", default=512)
+    args=parser.parse_args()
+    main(args)
